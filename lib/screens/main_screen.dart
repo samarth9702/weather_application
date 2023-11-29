9b974b283af38a24478dd6ca6917be6224a642df
samarth9702 <s223828778@deakin.edu.au>
2023-11-29 17:19:34 +1100

    Add: packages

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  String api = "21dcf044b7e20a02a0e1cdc6bc63a393";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
